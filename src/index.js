4cacfa80bc292442d475e5273c2df4e3973972c4
John Gerald Cayabyab <johngeraldcayabyab@gmail.com>
2021-04-30 22:50:32 +0800

    succesfully added light

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import * as THREE from 'THREE';
+import {GUI} from "dat.gui";
 
-function main(){
+function main() {
     const scene = new THREE.Scene();
+    const gui = new GUI();
 
     const fov = 75;
     const windowInnerWidth = window.innerWidth;
@@ -19,39 +21,66 @@ function main(){
 
 
     const geometry = new THREE.BoxGeometry(.5, .5, .5);
-    const material = new THREE.MeshBasicMaterial({color : 0x00FF00});
+    const material = new THREE.MeshPhongMaterial({color: 0x00FF00});
     const cube = new THREE.Mesh(geometry, material);
     // cube.position.x = 5;
 
     scene.add(cube);
 
     // wireframe
-    let geo = new THREE.EdgesGeometry( cube.geometry );
-    let mat = new THREE.LineBasicMaterial( { color: 0x000000, linewidth: 4 } );
-    let wireframe = new THREE.LineSegments( geo, mat );
+    let geo = new THREE.EdgesGeometry(cube.geometry);
+    let mat = new THREE.LineBasicMaterial({color: 0x000000, linewidth: 4});
+    let wireframe = new THREE.LineSegments(geo, mat);
     wireframe.renderOrder = 1; // make sure wireframes are rendered 2nd
-    cube.add( wireframe );
+    cube.add(wireframe);
 
     camera.position.z = 5;
 
+    const light = new THREE.PointLight(0xffffff, 5);
+    light.position.z = 5;
 
+    // light.shadow.camera.near = near;
+    // light.shadow.camera.far = far;
+    // light.shadow.camera.fov = fov;
+    scene.add(light);
 
+    class AxisGridHelper {
+        constructor(node, units = 10) {
+            const axes = new THREE.AxesHelper();
+            axes.material.depthTest = false;
+            axes.renderOrder = 2;
+            node.add(axes);
 
-    const spotLight = new THREE.SpotLight(0x000000);
-    spotLight.position.z = 5;
+            const grid = new THREE.GridHelper(units, units);
+            grid.material.depthTest = false;
+            grid.renderOrder = 1;
+            node.add(grid);
 
-    spotLight.castShadow = true;
+            this.grid = grid;
+            this.axes = axes;
+            this.visible = false;
+        }
 
-    spotLight.shadow.mapSize.width = windowInnerWidth;
-    spotLight.shadow.mapSize.height = windowInnerHeight;
-    //
-    spotLight.shadow.camera.near = near;
-    spotLight.shadow.camera.far = far;
-    spotLight.shadow.camera.fov = fov;
-    scene.add(spotLight);
+        get visible() {
+            return this._visible;
+        }
 
+        set visible(v) {
+            this._visible = v;
+            this.grid.visible = v;
+            this.axes.visible = v;
+        }
+    }
 
-    function animate(){
+    function makeAxisGrid(node, label, units) {
+        const helper = new AxisGridHelper(node, units);
+        gui.add(helper, 'visible').name(label);
+    }
+
+    makeAxisGrid(cube, 'cube');
+    makeAxisGrid(light, 'ambient light');
+
+    function animate() {
         requestAnimationFrame(animate);
         cube.rotation.x += 0.01;
         cube.rotation.y += 0.01;
