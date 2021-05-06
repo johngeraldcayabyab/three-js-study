58f9b6e41d0ce1942adeeccace16a75e196b91a8
John Gerald Cayabyab <johngeraldcayabyab@gmail.com>
2021-05-07 00:15:34 +0800

    experiment random lines

--- src/index.js
+++ src/index.js
@@ -1,29 +1,62 @@
 import * as THREE from 'three';
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 
-function main(){
+
+function main() {
+
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
-    const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
-    camera.position.set(0, 0 , 100);
+    const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 500);
+    camera.position.set(0, 0, 100);
     camera.lookAt(0, 0, 0);
 
     const scene = new THREE.Scene();
 
-    const material = new THREE.LineBasicMaterial({color : 0xEDFF9A});
+    const material = new THREE.LineBasicMaterial({color: 0xEDFF9A});
 
-    const points  = [];
+    const points = [];
 
-    points.push(new THREE.Vector3(10, 0, 0));
-    points.push(new THREE.Vector3(50, 0, 0));
 
+    for (let x = 0; x < 100; x++) {
+        points.push(new THREE.Vector3(x, Math.random() * 10, Math.random() * 10));
+    }
     const geometry = new THREE.BufferGeometry().setFromPoints(points);
 
     const line = new THREE.Line(geometry, material);
 
     scene.add(line);
-    renderer.render(scene, camera);
+
+    const cameraControls = new OrbitControls(camera, renderer.domElement);
+    cameraControls.update();
+
+    function resizeRendererToDisplaySize(renderer) {
+        const canvas = renderer.domElement;
+        const width = canvas.clientWidth;
+        const height = canvas.clientHeight;
+        const needResize = canvas.width !== width || canvas.height !== height;
+        if (needResize) {
+            renderer.setSize(width, height, false);
+        }
+        return needResize;
+    }
+
+
+    function render(time) {
+        time *= 0.001;
+        if (resizeRendererToDisplaySize(renderer)) {
+            const canvas = renderer.domElement;
+            camera.aspect = canvas.clientWidth / canvas.clientHeight;
+            camera.updateProjectionMatrix();
+        }
+        line.rotation.x += 0.01;
+        // line.rotation.y += 0.01;
+        renderer.render(scene, camera);
+        requestAnimationFrame(render);
+    }
+
+    requestAnimationFrame(render);
 }
 
 main();
