6870ea9263b22eba3e5aeb28b52148ca07686ed9
John Gerald Cayabyab <johngeraldcayabyab@gmail.com>
2021-05-16 22:03:02 +0800

    studying what a three.vector3 is and how fogs work

--- src/index.js
+++ src/index.js
@@ -1,149 +1,41 @@
 import * as THREE from 'three';
-import {Lensflare, LensflareElement} from "three/examples/jsm/objects/Lensflare";
-import {FlyControls} from "three/examples/jsm/controls/FlyControls";
-import Stats from 'stats.js/src/Stats.js';
-import {CinematicCamera} from "three/examples/jsm/cameras/CinematicCamera";
-import {GUI} from "dat.gui";
+import Stats from 'stats.js';
+import {GUI} from 'dat.gui';
+import {GLTFLoaderF} from "three/examples/jsm/loaders/GLTFLoader";
 
-let camera, scene, raycaster, renderer, stats;
+let container, stats, clock, gui, mixer, actions, activeAction, previousAction;
+let camera, scene, renderer, model, face;
 
-const mouse = new THREE.Vector2();
-let INTERSECTED;
-const radius = 100;
-let theta = 0;
+const api = {state: 'walking'};
 
 init();
 animate();
 
 function init() {
-    camera = new CinematicCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
-    camera.setLens(5);
-    camera.position.set(2, 1, 500);
+
+    console.log(new THREE.Vector3(0, 2, 0));
+
+    container = document.createElement('div');
+    document.body.appendChild(container);
+
+    camera = new THREE.PerspectiveCamera(45, window.innerHeight / window.innerHeight, 0.25, 100);
+    camera.position.set(-5, 3, 10);
+    camera.lookAt(new THREE.Vector3(0, 2, 0));
 
     scene = new THREE.Scene();
-    scene.background = new THREE.Color(0xf0f0f0);
+    scene.background = new THREE.Color(0x0e0e0e);
+    scene.fog = new THREE.Fog(0x0e0e0e, 20, 100);
 
-    scene.add(new THREE.AmbientLight(0xffffff, 0.3));
+    clock = new THREE.Clock();
 
-    const light = new THREE.DirectionalLight(0xffffff, 0.35);
-    light.position.set(1, 1, 1).normalize();
-    scene.add(light);
+    const hemLight = new THREE.HemisphereLight(0xffffff, 0x444444);
+    hemLight.position.set(0, 20, 0);
+    scene.add(hemLight);
 
-    const geometry = new THREE.BoxGeometry(20, 20, 20);
-
-    for (let i = 0; i < 100; i++) {
-        const object = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial({color: Math.random() * 0xffffff}));
-
-        object.position.x = Math.random() * 800 - 400;
-        object.position.y = Math.random() * 800 - 400;
-        object.position.z = Math.random() * 800 - 400;
-
-        scene.add(object);
-    }
-
-    raycaster = new THREE.Raycaster();
-
-    renderer = new THREE.WebGLRenderer({antialias: true});
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
-
-    stats = new Stats();
-    document.body.appendChild(stats.dom);
-
-    document.addEventListener('mousemove', onDocumentMouseMove);
-
-    window.addEventListener('resize', onWindowResize);
-
-    const effectController = {
-        focalLength: 15,
-        fstop: 2.8,
-        showFocus: false,
-        focalDepth: 3,
-    };
-
-    const matChanger = function () {
-        for (const e in effectController) {
-            if (e in camera.postprocessing.bokeh_uniforms) {
-                camera.postprocessing.bokeh_uniforms[e].value = effectController[e];
-            }
-        }
-
-        camera.postprocessing.bokeh_uniforms['znear'].value = camera.near;
-        camera.postprocessing.bokeh_uniforms['zfar'].value = camera.far;
-        camera.setLens(effectController.focalLength, camera.frameHeight, effectController.fstop, camera.coc);
-        effectController['focalDepth'] = camera.postprocessing.bokeh_uniforms['focalDepth'].value;
-    };
-
-    const gui = new GUI;
-
-    gui.add(effectController, 'focalLength', 1, 135, 0.01).onChange(matChanger);
-    gui.add(effectController, 'fstop', 1.8, 22, 0.01).onChange(matChanger);
-    gui.add(effectController, 'focalDepth', 0.1, 100, 0.001).onChange(matChanger);
-    gui.add(effectController, 'showFocus', true).onChange(matChanger);
-
-    matChanger();
-
-    window.addEventListener('resize', onWindowResize);
-}
-
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize(window.innerWidth, window.innerHeight);
-}
-
-function onDocumentMouseMove() {
-    event.preventDefault();
-
-    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+    const dirLight = new THREE.DirectionalLight(0xffffff);
+    dirLight.position.set(0, 20, 10);
 }
 
 function animate() {
-    requestAnimationFrame(animate, renderer.domElement);
 
-    render();
-    stats.update();
-}
-
-function render() {
-    theta += 0.1;
-
-    camera.position.x = radius * Math.sin(THREE.MathUtils.degToRad(theta));
-    camera.position.y = radius * Math.sin(THREE.MathUtils.degToRad(theta));
-    camera.position.z = radius * Math.cos(THREE.MathUtils.degToRad(theta));
-
-    camera.updateMatrixWorld();
-
-    raycaster.setFromCamera(mouse, camera);
-
-    const intersects = raycaster.intersectObjects(scene.children);
-
-    if (intersects.length > 0) {
-        const targetDistance = intersects[0].distance;
-
-        camera.focusAt(targetDistance);
-
-        if (INTERSECTED != intersects[0].object) {
-            if (INTERSECTED) INTERSECTED.material.emissive.setHex(INTERSECTED.currentHex);
-
-            INTERSECTED = intersects[0].object;
-            INTERSECTED.currentHex = INTERSECTED.material.emissive.getHex();
-            INTERSECTED.material.emissive.setHex(0xff0000);
-        }
-    } else {
-        if (INTERSECTED) INTERSECTED.material.emissive.setHex(INTERSECTED.currentHex);
-        INTERSECTED = null;
-    }
-
-    if (camera.postprocessing.enabled) {
-        camera.renderCinematic(scene, renderer);
-    } else {
-        scene.overrideMaterial = null;
-
-        renderer.clear();
-        renderer.render(scene, camera);
-    }
 }
