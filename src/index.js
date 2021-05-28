596889049a559f3c82e7984087316fc60b13cdd5
John Gerald Cayabyab <johngeraldcayabyab@gmail.com>
2021-05-28 22:57:30 +0800

    understanding how canvas works part 2

--- src/index.js
+++ src/index.js
@@ -25,95 +25,43 @@ import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 // console.log(noise.noise(20, 30));
 //
 
-function main() {
-
-    let container, stats, camera, scene, renderer, controls;
-
-    const clock = new THREE.Clock();
-    const windowInnerWidth = window.innerWidth;
-    const windowInnerHeight = window.innerHeight;
-
-
-    init();
-    animate();
-
-    function init() {
-        container = document.createElement('div');
-        document.body.appendChild(container);
-
-        camera = new THREE.PerspectiveCamera(desktopFOV(), windowInnerWidth / windowInnerHeight, 1, 40000);
-        camera.position.z = 250;
-        camera.lookAt(0, 0, 0);
-
-        scene = new THREE.Scene();
-        scene.background = new THREE.Color().setHSL(0.51, 0.4, 0.01);
-        scene.fog = new THREE.Fog(scene.background, 1, 40000)
-
-        renderer = new THREE.WebGLRenderer({antialias: true});
-        renderer.setPixelRatio(window.devicePixelRatio);
-        renderer.setSize(windowInnerWidth, windowInnerHeight);
-        renderer.outputEncoding = THREE.sRGBEncoding;
-
-
-
-        container.appendChild(renderer.domElement);
-
-        // const canvas = renderer.domElement;
-        // // console.log(canvas);
-        // const ctx = canvas.getContext('3d');
-        // console.log(ctx );
-        // // ctx.fillStyle = 'green';
-        // // ctx.fillRect(10, 10, 100, 100);
-
-
-
-        controls = new OrbitControls(camera, renderer.domElement);
-
-        scene.add(createPlane());
-
-        stats = new Stats();
-        container.appendChild(stats.dom);
-        window.addEventListener('resize', onWindowResize);
-    }
-
-    function createLine(){
-        const material = new THREE.LineBasicMaterial({color : 0xEDFF9A});
-        const points  = [];
-        points.push(new THREE.Vector3(10, 0, 0));
-        points.push(new THREE.Vector3(50, 0, 0));
-        const geometry = new THREE.BufferGeometry().setFromPoints(points);
-        return new THREE.Line(geometry, material);
-    }
-
-    function createPlane(){
-        const geometry = new THREE.PlaneGeometry( 500, 500 );
-        const material = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
-        return new THREE.Mesh( geometry, material );
-    }
-
-    function animate() {
-        requestAnimationFrame(animate);
-        render();
-        stats.update();
-    }
-
-    function onWindowResize() {
-        renderer.setSize(windowInnerWidth, windowInnerHeight);
-        camera.aspect = windowInnerWidth / windowInnerHeight;
-        camera.updateProjectionMatrix();
-    }
-
-    function render() {
-        const delta = clock.getDelta();
-        controls.update(delta);
-        renderer.render(scene, camera);
-    }
-}
-
-main();
-
 
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
-ctx.fillStyle = 'green';
-ctx.fillRect(10, 10, 100, 100);
+// ctx.fillStyle = 'green';
+// ctx.fillRect(100, 100, 800, 800);
+
+ctx.fillStyle = 'red';
+ctx.fillRect(0, 0, 5, 5);
+
+ctx.fillStyle = 'blue';
+ctx.fillRect(5, 0, 5, 5);
+
+ctx.fillStyle = 'red';
+ctx.fillRect(10, 0, 5, 5);
+
+for(let i = 0; i < 800; i++){
+
+}
+
+//
+// let canvas = document.getElementById('canvas');
+// let ctx = canvas.getContext('2d');
+// let canvasWidth = canvas.width;
+// let canvasHeight = canvas.height;
+// ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+// let id = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
+// let pixels = id.data;
+//
+// let x = Math.floor(Math.random() * canvasWidth);
+// let y = Math.floor(Math.random() * canvasHeight);
+// let r = Math.floor(Math.random() * 256);
+// let g = Math.floor(Math.random() * 256);
+// let b = Math.floor(Math.random() * 256);
+// let off = (y * id.width + x) * 4;
+// pixels[off] = r;
+// pixels[off + 1] = g;
+// pixels[off + 2] = b;
+// pixels[off + 3] = 255;
+//
+// ctx.putImageData(id, 0, 0);
