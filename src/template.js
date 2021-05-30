3aaa57d102669254cf4561518557c4d93713c080
John Gerald Cayabyab <johngeraldcayabyab@gmail.com>
2021-05-30 11:08:32 +0800

    tried to integrated the moving tank with generated terrain. failed

--- src/template.js
+++ src/template.js
@@ -3,58 +3,58 @@ import Stats from 'stats.js';
 import {desktopFOV} from "./FIeldOfViews";
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 
-function main() {
-    let container, stats, camera, scene, renderer, controls;
 
-    const clock = new THREE.Clock();
-    const windowInnerWidth = window.innerWidth;
-    const windowInnerHeight = window.innerHeight;
+let container, stats, camera, scene, renderer, controls;
 
-    init();
-    animate();
+const clock = new THREE.Clock();
+const windowInnerWidth = window.innerWidth;
+const windowInnerHeight = window.innerHeight;
 
-    function init() {
-        container = document.createElement('div');
-        document.body.appendChild(container);
+init();
+animate();
 
-        camera = new THREE.PerspectiveCamera(desktopFOV(), windowInnerWidth / windowInnerHeight, 1, 40000);
-        camera.position.z = 250;
-        camera.lookAt(0, 0, 0);
+function init() {
+    container = document.createElement('div');
+    document.body.appendChild(container);
 
-        scene = new THREE.Scene();
-        scene.background = new THREE.Color().setHSL(0.51, 0.4, 0.01);
-        scene.fog = new THREE.Fog(scene.background, 1, 40000)
+    camera = new THREE.PerspectiveCamera(desktopFOV(), windowInnerWidth / windowInnerHeight, 1, 40000);
+    camera.position.z = 250;
+    camera.lookAt(0, 0, 0);
 
-        renderer = new THREE.WebGLRenderer({antialias: true});
-        renderer.setPixelRatio(window.devicePixelRatio);
-        renderer.setSize(windowInnerWidth, windowInnerHeight);
-        renderer.outputEncoding = THREE.sRGBEncoding;
-        container.appendChild(renderer.domElement);
+    scene = new THREE.Scene();
+    // scene.background = new THREE.Color().setHSL(0.51, 0.4, 0.01);
+    // scene.fog = new THREE.Fog(scene.background, 1, 40000)
 
-        controls = new OrbitControls(camera, renderer.domElement);
+    renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(windowInnerWidth, windowInnerHeight);
+    renderer.outputEncoding = THREE.sRGBEncoding;
+    container.appendChild(renderer.domElement);
 
-        stats = new Stats();
-        container.appendChild(stats.dom);
-        window.addEventListener('resize', onWindowResize);
-    }
+    controls = new OrbitControls(camera, renderer.domElement);
 
-    function animate() {
-        requestAnimationFrame(animate);
-        render();
-        stats.update();
-    }
-
-    function onWindowResize() {
-        renderer.setSize(windowInnerWidth, windowInnerHeight);
-        camera.aspect = windowInnerWidth / windowInnerHeight;
-        camera.updateProjectionMatrix();
-    }
-
-    function render() {
-        const delta = clock.getDelta();
-        controls.update(delta);
-        renderer.render(scene, camera);
-    }
+    stats = new Stats();
+    container.appendChild(stats.dom);
+    window.addEventListener('resize', onWindowResize);
 }
 
-main();
+function animate() {
+    requestAnimationFrame(animate);
+    render();
+    stats.update();
+}
+
+function onWindowResize() {
+    renderer.setSize(windowInnerWidth, windowInnerHeight);
+    camera.aspect = windowInnerWidth / windowInnerHeight;
+    camera.updateProjectionMatrix();
+}
+
+function render() {
+    const delta = clock.getDelta();
+    controls.update(delta);
+    renderer.render(scene, camera);
+}
+
+
+
