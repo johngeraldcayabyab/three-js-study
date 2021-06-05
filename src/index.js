544856979ae69127e4f97f4f2cc061f08babd9cb
John Gerald Cayabyab <johngeraldcayabyab@gmail.com>
2021-06-05 08:25:47 +0800

    going back to terrain copying

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import * as THREE from 'three';
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
-import {ImprovedNoise} from "three/examples/jsm/math/ImprovedNoise";
+import {generateHeight, generateTexture} from "./helpers";
+import Stats from 'stats.js/src/Stats';
 
 
 main();
 
 function main() {
-
     let container, stats;
 
     let camera, controls, scene, renderer;
@@ -17,11 +17,8 @@ function main() {
 
     let helper;
 
-
     const raycaster = new THREE.Raycaster();
     const pointer = new THREE.Vector2();
-    const windowInnerWidth = window.innerWidth;
-    const windowInnerHeight = window.innerHeight;
 
     init();
     animate();
@@ -35,13 +32,13 @@ function main() {
 
         renderer = new THREE.WebGLRenderer({antialias: true});
         renderer.setPixelRatio(window.devicePixelRatio);
-        renderer.setSize(windowInnerWidth, windowInnerHeight);
+        renderer.setSize(window.innerWidth, window.innerHeight);
         container.appendChild(renderer.domElement);
 
         scene = new THREE.Scene();
         scene.background = new THREE.Color(0xbfd1e5);
 
-        camera = new THREE.PerspectiveCamera(60, windowInnerWidth / windowInnerHeight, 10, 20000);
+        camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 10, 20000);
 
         controls = new OrbitControls(camera, renderer.domElement);
         controls.minDistance = 1000;
@@ -49,36 +46,75 @@ function main() {
         controls.maxPolarAngle = Math.PI / 2;
 
         const data = generateHeight(worldWidth, worldDepth);
-    }
 
-    function generateHeight(width, height) {
-        const size = width * height, data = new Uint8Array(size),
-            perlin = new ImprovedNoise(), z = Math.random() * 100;
+        controls.target.y = data[worldHalfWidth + worldHalfDepth * worldWidth] + 500;
+        camera.position.y = controls.target.y + 2000;
+        camera.position.x = 2000;
+        controls.update();
 
-        console.log(data);
+        const geometry = new THREE.PlaneGeometry(7500, 7500, worldWidth - 1, worldDepth - 1);
+        geometry.rotateX(-Math.PI / 2);
 
+        const vertices = geometry.attributes.position.array;
 
-        let quality = 1;
-        for (let j = 0; j < 4; j++) {
-            for (let i = 0; i < size; i++) {
-                const x = i % width, y = ~~(i / width);
-                data[i] += Math.abs(perlin.noise(x / quality, y / quality, z) * quality * 1.75);
-            }
-            quality *= 5;
+        for (let i = 0, j = 0, l = vertices.length; i < l; i++, j += 3) {
+            vertices[j + 1] = data[i] * 10;
         }
-        return data;
+
+        geometry.computeFaceNormals();
+
+        texture = new THREE.CanvasTexture(generateTexture((data, worldWidth, worldDepth)));
+        texture.wrapS = THREE.ClampToEdgeWrapping;
+        texture.wrapT = THREE.ClampToEdgeWrapping;
+
+        mesh = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({map: texture}));
+        scene.add(mesh);
+
+        const geometryHelper = new THREE.ConeGeometry(20, 100, 3);
+        geometryHelper.translate(0, 50, 0);
+        geometryHelper.rotateX(Math.PI / 2);
+        helper = new THREE.Mesh(geometryHelper, new THREE.MeshNormalMaterial());
+        scene.add(helper);
+
+        container.addEventListener('pointermove', onPointerMove);
+
+        stats = new Stats();
+        container.appendChild(stats.dom);
+
+        window.addEventListener('resize', onWindowResize);
     }
+
 
     function animate() {
-
+        requestAnimationFrame(animate);
+        render();
+        stats.update();
     }
 
     function onWindowResize() {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
 
+        renderer.setSize(window.innerWidth, window.innerHeight);
     }
 
-    function render() {
 
+    function render() {
+        renderer.render(scene, camera);
+    }
+
+    function onPointerMove(event) {
+        pointer.x = (event.clientX / renderer.domElement.clientWidth) * 2 - 1;
+        pointer.y = -(event.clientY / renderer.domElement.clientHeight) * 2 + 1;
+        raycaster.setFromCamera(pointer, camera);
+
+        const intersects = raycaster.intersectObject(mesh);
+
+        if (intersects.length > 0) {
+            helper.position.set(0, 0, 0);
+            helper.lookAt(intersects[0].face.normal);
+            helper.position.copy(intersects[0].point);
+        }
     }
 }
 
