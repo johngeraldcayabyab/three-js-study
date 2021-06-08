6e90774d1ce0410fb3e600f61eddac22f7f616cb
John Gerald Cayabyab <johngeraldcayabyab@gmail.com>
2021-06-08 09:07:05 +0800

    understanding geometry instancing part 1

--- src/index.js
+++ src/index.js
@@ -1,90 +1,42 @@
-import * as THREE from 'three';
-import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
-import Stats from 'stats.js/src/Stats';
-import {createContainer, createRenderer} from "./helpers.js";
-
-main();
+import * as THREE from 'three/src/Three';
+import {createContainer, createRenderer} from "./helpers";
 
 function main() {
 
-    let container, renderer, scene, camera, controls, stats, INTERSECTED;
+    let container, renderer, scene, camera, controls;
+
+    const amount = parseInt(window.location.search.substr(1)) || 10;
+    const count = Math.pow(amount, 3);
 
     const raycaster = new THREE.Raycaster();
     const pointer = new THREE.Vector2();
 
-    init();
     animate();
 
     function init() {
         container = createContainer(container);
-        renderer = createRenderer(renderer, container);
+        renderer = createRenderer(renderer);
 
-        scene = new THREE.Scene();
-        scene.background = '#000';
-
-        const light = new THREE.DirectionalLight(0xffffff, 1);
-        light.position.set(1, 1, 1).normalize();
-        scene.add(light);
-
-        camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 20000);
-        camera.lookAt(0, 0, 0);
-        camera.position.y = 7000;
-        camera.position.z = 500;
-
-        controls = new OrbitControls(camera, renderer.domElement);
-        controls.update();
-
-        scene.add(createPlane());
-        scene.add(createBox());
-
-        container.addEventListener('pointermove', onPointerMove);
-        stats = new Stats();
-        container.appendChild(stats.dom);
+        camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0, 1, 2000);
+        camera.position.set(amount, amount, amount);
     }
 
-    function createPlane() {
-        const geometry = new THREE.PlaneGeometry(7500, 7500, 256 - 1, 256 - 1);
-        geometry.rotateX(-Math.PI / 2);
-        const material = new THREE.MeshPhongMaterial({color: 0xbfd1e5, side: THREE.DoubleSide});
-        return new THREE.Mesh(geometry, material);
-    }
-
-    function createBox() {
-        const geometry = new THREE.BoxGeometry(50, 50, 50);
-        const material = new THREE.MeshPhongMaterial({color: 0x00ff00});
-        return new THREE.Mesh(geometry, material);
-    }
-
-    function animate() {
+    function animate(time) {
         requestAnimationFrame(animate);
         render();
-        stats.update();
-    }
-
-    function onPointerMove(event) {
-        pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
-        pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
-    }
-
-    function findIntersections() {
-        raycaster.setFromCamera(pointer, camera);
-        const intersects = raycaster.intersectObjects(scene.children);
-        if (intersects.length > 0) {
-            if (INTERSECTED !== intersects[0].object) {
-                if (INTERSECTED) {
-                    INTERSECTED.material.emissive.setHex(INTERSECTED.currentHex)
-                }
-                INTERSECTED = intersects[0].object;
-                INTERSECTED.currentHex = INTERSECTED.material.emissive.getHex();
-                INTERSECTED.material.emissive.setHex(0xff0000);
-            }
-        }
     }
 
     function render() {
-        findIntersections();
-        renderer.render(scene, camera);
+
+    }
+
+    function onWindowResize() {
+
+    }
+
+
+    function onPointerMove() {
+
     }
 }
-
 
