3e95790926214e26cf957b0a6028dd767e1e456b
John Gerald Cayabyab <johngeraldcayabyab@gmail.com>
2021-06-14 20:33:35 +0800

    the canvas doesnt need a container

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import {createContainer, createRenderer, onWindowResize} from "./helpers";
 import {GeometryUtils} from "three/examples/jsm/utils/GeometryUtils";
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 
+
 main();
 
 function main() {
-    let container, renderer, scene, camera;
+
+    let container, camera, scene, renderer;
     let line, sprite, texture;
+
     let cameraOrtho, sceneOrtho;
+
     let offset = 0;
 
     const dpr = window.devicePixelRatio;
@@ -21,8 +25,9 @@ function main() {
     animate();
 
     function init() {
+
         container = createContainer(container);
-        renderer = createRenderer(renderer, container);
+        //
 
         const width = window.innerWidth;
         const height = window.innerHeight;
@@ -35,6 +40,8 @@ function main() {
 
         scene = new THREE.Scene();
         sceneOrtho = new THREE.Scene();
+
+        //
 
         const points = GeometryUtils.gosper(8);
 
@@ -53,11 +60,15 @@ function main() {
         line.scale.setScalar(0.05);
         scene.add(line);
 
+        //
+
         const data = new Uint8Array(textureSize * textureSize * 3);
 
-        texture = new THREE.DataTexture(data, textureSize, textureSize, THREE.RGBAFormat);
+        texture = new THREE.DataTexture(data, textureSize, textureSize, THREE.RGBFormat);
         texture.minFilter = THREE.NearestFilter;
         texture.magFilter = THREE.NearestFilter;
+
+        //
 
         const spriteMaterial = new THREE.SpriteMaterial({map: texture});
         sprite = new THREE.Sprite(spriteMaterial);
@@ -66,11 +77,29 @@ function main() {
 
         updateSpritePosition();
 
+        // renderer = createRenderer(renderer, container);
+
+        //
+        //
+        renderer = new THREE.WebGLRenderer({antialias: true});
+        renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.autoClear = false;
+        console.log(renderer.domElement);
+        document.body.appendChild(renderer.domElement);
+
+        //
+
         const selection = document.getElementById('selection');
         const controls = new OrbitControls(camera, selection);
         controls.enablePan = false;
 
+
+
+        //
+
         window.addEventListener('resize', onWindowResize);
+
     }
 
     function onWindowResize() {
@@ -94,6 +123,7 @@ function main() {
     }
 
     function updateSpritePosition() {
+
         const halfWidth = window.innerWidth / 2;
         const halfHeight = window.innerHeight / 2;
 
@@ -101,16 +131,22 @@ function main() {
         const halfImageHeight = textureSize / 2;
 
         sprite.position.set(-halfWidth + halfImageWidth, halfHeight - halfImageHeight, 1);
+
     }
 
     function animate() {
+
         requestAnimationFrame(animate);
 
         const colorAttribute = line.geometry.getAttribute('color');
         updateColors(colorAttribute);
 
+        // scene rendering
+
         renderer.clear();
         renderer.render(scene, camera);
+
+        // calculate start position for copying data
 
         vector.x = (window.innerWidth * dpr / 2) - (textureSize / 2);
         vector.y = (window.innerHeight * dpr / 2) - (textureSize / 2);
@@ -119,19 +155,28 @@ function main() {
 
         renderer.clearDepth();
         renderer.render(sceneOrtho, cameraOrtho);
+
     }
 
     function updateColors(colorAttribute) {
+
         const l = colorAttribute.count;
+
         for (let i = 0; i < l; i++) {
-            const h = ((offset + 1) % l) / l;
+
+            const h = ((offset + i) % l) / l;
+
             color.setHSL(h, 1, 0.5);
             colorAttribute.setX(i, color.r);
             colorAttribute.setY(i, color.g);
             colorAttribute.setZ(i, color.b);
+
         }
+
         colorAttribute.needsUpdate = true;
+
         offset -= 25;
+
     }
 }
 
