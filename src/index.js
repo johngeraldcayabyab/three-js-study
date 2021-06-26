5f4ff12ce352e1cab6e4846b28d0ada545191c61
John Gerald Cayabyab <johngeraldcayabyab@gmail.com>
2021-06-26 15:31:14 +0800

    still trying to understand what uintarray is, so i'll try to make a bit to number/character parser later

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import {
 } from "./helpers";
 
 import * as THREE from 'three';
+import {ImprovedNoise} from "three/examples/jsm/math/ImprovedNoise";
 
 main();
 
@@ -44,7 +45,48 @@ function main() {
 
         const size = 128
         const data = new Uint8Array(size * size * size);
-        console.log(data);
+
+        let i = 0;
+        const scale = 0.05;
+        const perlin = new ImprovedNoise();
+        const vector = new THREE.Vector3();
+
+        for (let z = 0; z < size; z++) {
+            for (let y = 0; y < size; y++) {
+                for (let x = 0; x < size; x++) {
+                    const d = 1.0 - vector.set(x, y, z).subScalar(size / 2).divideScalar(size).length();
+                    data[i] = (128 + 128 * perlin.noise(x * scale / 1.5, y * scale, z * scale / 1.5)) * d * d;
+                    i++;
+                }
+            }
+        }
+
+        const texture = new THREE.DataTexture3D(data, size, size, size);
+        texture.format = THREE.RedFormat;
+        texture.minFilter = THREE.LinearFilter;
+        texture.magFilter = THREE.LinearFilter;
+        texture.unpackAlignment = 1;
+
+        const vertexShader = `
+            in vec3 position;
+            
+            uniform mat4 modelMatrix;
+            uniform mat4 modelViewMatrix;
+            uniform mat4 projectionMatrix;
+            uniform vec3 cameraPos;
+            
+            out vec3 vOrigin;
+            out vec3 vDirection;
+            
+            void main(){
+                vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);
+                
+                vOrigin = vec3(inverse(modelMatrix) * vec4(cameraPos, 1.0)).xyz;
+                vDirection = position - vOrigin
+                
+                gl_Position = projectionMatrix * mvPosition;
+            }
+        `;
 
 
         window.addEventListener('resize', onWindowResize);
