ffa00a9ba84b66acf4549015d8bbd569cdfc37ec
John Gerald Cayabyab <johngeraldcayabyab@gmail.com>
2021-07-01 18:34:24 +0800

    created a file for reusable primitive objects

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import {
 } from "./helpers";
 
 import * as THREE from 'three';
+import {createPineTree, createPlane} from "./object_generator";
 
 main();
 
@@ -21,45 +22,15 @@ function main() {
         container = createContainer(container);
         renderer = createRenderer(renderer, container);
         scene = createScene(scene);
-        camera = createPerspectiveCamera(camera, {x : 10, y: 5, z:10});
+        camera = createPerspectiveCamera(camera, {x: 10, y: 5, z: 10});
         controls = createControls(controls, camera, renderer);
         stats = createStats(stats, container);
 
+        const plane = createPlane();
+        scene.add(plane);
 
-
-        const group = new THREE.Group();
-
-        const level1 = new THREE.Mesh(
-            new THREE.ConeGeometry(1.5, 2,8),
-            new THREE.MeshLambertMaterial({color: 0x000ff00})
-        );
-        level1.position.y = 4;
-        group.add(level1);
-
-        const level2 = new THREE.Mesh(
-            new THREE.ConeGeometry(2,2,8),
-            new THREE.MeshLambertMaterial({color: 0x00ff00})
-        );
-        level2.position.y = 3;
-        group.add(level2);
-
-        const level3 = new THREE.Mesh(
-            new THREE.ConeGeometry(3,2,8),
-            new THREE.MeshLambertMaterial({color: 0x00ff00})
-        );
-        level3.position.y = 2;
-        group.add(level3);
-
-        const trunk = new THREE.Mesh(
-            new THREE.CylinderGeometry(0.5, 0.5, 2),
-            new THREE.MeshLambertMaterial({color: 0xbb6600})
-        );
-        trunk.position.y = 0;
-        group.add(trunk);
-
-
-        scene.add(group);
-
+        const pineTree = createPineTree();
+        scene.add(pineTree);
 
 
         const ambientLight = new THREE.AmbientLight(0xcccccc, 0.4);
@@ -68,17 +39,6 @@ function main() {
         const pointLight = new THREE.PointLight(0xffffff, 0.8);
         camera.add(pointLight);
         scene.add(camera);
-
-
-        let createPlane = () => {
-            const geometry = new THREE.PlaneGeometry(500, 500, 32);
-            const material = new THREE.MeshLambertMaterial({color: 0x6584b5, side: THREE.DoubleSide});
-            return new THREE.Mesh(geometry, material);
-        };
-
-        const plane = createPlane();
-        plane.rotation.x = 300;
-        scene.add(plane);
 
 
         container.addEventListener('resize', onWindowResize);
---
+++ src/object_generator.js
@@ -0,0 +1,43 @@
+import * as THREE from '../node_modules/three/build/three.module.js';
+
+export const createPlane = () => {
+    const geometry = new THREE.PlaneGeometry(500, 500, 32);
+    const material = new THREE.MeshLambertMaterial({color: 0x6584b5, side: THREE.DoubleSide});
+    const mesh = new THREE.Mesh(geometry, material);
+    mesh.rotation.x = 300;
+    return mesh;
+};
+
+export const createPineTree = () => {
+    const group = new THREE.Group();
+
+    const level1 = new THREE.Mesh(
+        new THREE.ConeGeometry(1.5, 2, 8),
+        new THREE.MeshLambertMaterial({color: 0x000ff00})
+    );
+    level1.position.y = 5;
+    group.add(level1);
+
+    const level2 = new THREE.Mesh(
+        new THREE.ConeGeometry(2, 2, 8),
+        new THREE.MeshLambertMaterial({color: 0x00ff00})
+    );
+    level2.position.y = 4;
+    group.add(level2);
+
+    const level3 = new THREE.Mesh(
+        new THREE.ConeGeometry(3, 2, 8),
+        new THREE.MeshLambertMaterial({color: 0x00ff00})
+    );
+    level3.position.y = 3;
+    group.add(level3);
+
+    const trunk = new THREE.Mesh(
+        new THREE.CylinderGeometry(0.5, 0.5, 2),
+        new THREE.MeshLambertMaterial({color: 0xbb6600})
+    );
+    trunk.position.y = 1;
+    group.add(trunk);
+
+    return group;
+};
