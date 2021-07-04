b10545be1062d6e60a17fa0264008c89064ea24c
John Gerald Cayabyab <johngeraldcayabyab@gmail.com>
2021-07-05 01:23:44 +0800

    plane now flies, but it moves backward and the propeller's not working

--- src/index.js
+++ src/index.js
@@ -15,6 +15,19 @@ main();
 function main() {
     let container, renderer, scene, camera, controls, stats;
 
+    let t;
+    const n0 = new THREE.Vector3(0, 1, 0);
+    const n = new THREE.Vector3();
+    const b = new THREE.Vector3();
+    const M3 = new THREE.Matrix3();
+    const M4 = new THREE.Matrix4();
+    let f = 0;
+    let p = new THREE.Vector3();
+    let i = 0;
+
+    let curve;
+    let airplane;
+
     init();
     animate();
 
@@ -30,43 +43,38 @@ function main() {
         directionalLight.position.set(0, 6, 6);
         scene.add(directionalLight);
 
-
-
-        const hlp = new THREE.GridHelper(2000, 20);
+        const hlp = new THREE.GridHelper(1000, 20);
         scene.add(hlp);
 
-        let t;
-        const n0 = new THREE.Vector3(0, 1, 0);
-        const n = new THREE.Vector3();
-        const b = new THREE.Vector3();
-        const M3 = new THREE.Matrix3();
-        const M4 = new THREE.Matrix4();
-        let f = 0;
-        let p = new THREE.Vector3();
 
         const somePoints = [
-            new THREE.Vector3(-1000, 0, -1000),
-            new THREE.Vector3(0, 0, -0.8),
+            new THREE.Vector3(-500, 0, -500),
+            new THREE.Vector3(0, 0, -400),
 
-            new THREE.Vector3(1000, 0.2, -1000),
-            new THREE.Vector3(0.8, 0.1, 0),
+            new THREE.Vector3(500, 100, -500),
+            new THREE.Vector3(400, 100, 0),
 
-            new THREE.Vector3(-1000, 0.4, 1000),
-            new THREE.Vector3(-0.8, 0.2, 0),
+            new THREE.Vector3(500, 0, 500),
+            new THREE.Vector3(0, 100, 400),
+
+            new THREE.Vector3(-500, 0.4, 500),
+            new THREE.Vector3(-400, 100, 0),
         ];
 
-        const pts = new THREE.Points(new THREE.BufferGeometry().setFromPoints(somePoints), new THREE.PointsMaterial({color: "white", size: 0.04}));
+        const pts = new THREE.Points(new THREE.BufferGeometry().setFromPoints(somePoints), new THREE.PointsMaterial({
+            color: "white",
+            size: 20
+        }));
         scene.add(pts);
 
-        const curve = new THREE.CatmullRomCurve3(somePoints);
+        curve = new THREE.CatmullRomCurve3(somePoints);
         curve.closed = true;
 
         const points = curve.getPoints(80);
         const line = new THREE.LineLoop(new THREE.BufferGeometry().setFromPoints(points), new THREE.LineBasicMaterial({color: 0xffffaa}));
         scene.add(line);
 
-        const airplane = createAirplane();
-
+        airplane = createAirplane();
         scene.add(airplane);
 
         container.addEventListener('resize', onWindowResize);
@@ -79,6 +87,27 @@ function main() {
     }
 
     function animate() {
+
+        if (f === 0 || f > 1) {
+            n.copy(n0);
+            f = 0;
+        }
+
+        f += 0.0004;
+
+        t = curve.getTangent(f);
+        b.crossVectors(t, n);
+        n.crossVectors(t, b.negate());
+
+        M3.set(-t.x, n.x, b.x, -t.y, n.y, b.y, -t.z, n.z, b.z);
+        M4.setFromMatrix3(M3);
+
+        p = curve.getPoint(f);
+
+
+        airplane.setRotationFromMatrix(M4);
+        airplane.position.set(p.x, p.y, p.z);
+
         render();
         stats.update();
         requestAnimationFrame(animate);
