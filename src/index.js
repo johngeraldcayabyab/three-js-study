21e49870ca16384c1ad0297d41349d37f32423df
John Gerald Cayabyab <johngeraldcayabyab@gmail.com>
2021-07-05 17:14:11 +0800

    copied a sunken sphere, will try to combine it with the animated vertices

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ main();
 function main() {
     let container, renderer, scene, camera, controls, stats;
 
+    let gpl;
 
     init();
     animate();
@@ -22,7 +23,7 @@ function main() {
         container = createContainer(container);
         renderer = createRenderer(renderer, container);
         scene = createScene(scene);
-        camera = createPerspectiveCamera(camera, {x: 120, y: 100, z: 400});
+        camera = createPerspectiveCamera(camera, {x: 5, y: 5, z: 20});
         controls = createControls(controls, camera, renderer);
         stats = createStats(stats, container);
 
@@ -30,8 +31,54 @@ function main() {
         directionalLight.position.set(0, 6, 6);
         scene.add(directionalLight);
 
+        let gpt = new THREE.IcosahedronGeometry(5, 7);
+        let mpt = new THREE.PointsMaterial({size: 0.15, color: 0x00ffff});
+        let pt = new THREE.Points(gpt, mpt);
+        scene.add(pt);
+
+        gpl = new THREE.PlaneGeometry(20, 20, 100, 100);
+        gpl.rotateX(Math.PI * -0.5);
+
+        bendThePlane(5, 3, 10);
+
+        let mpl = new THREE.PointsMaterial({size: 0.05, color: 0xaaaaaa});
+        let pl = new THREE.Points(gpl, mpl);
+        scene.add(pl);
+
 
         container.addEventListener('resize', onWindowResize);
+    }
+
+    function bendThePlane(radius, pHeight, smoothness) {
+        let pos = gpl.attributes.position;
+        let v3 = new THREE.Vector3();
+        for (let i = 0; i < pos.count; i++) {
+            v3.fromBufferAttribute(pos, i);
+            let a = pHeight;
+            let b = getSphereCone(v3, -3, radius);
+            let sm = smoothFunc(a, b, smoothness);
+            pos.setY(i, sm);
+        }
+    }
+
+    function getSphereCone(p, h, r) {
+        let dist = Math.hypot(p.x, p.z);
+        let limR = Math.sqrt(r * r - h * h);
+
+        let res;
+        if (dist <= limR) {
+            res = Math.sqrt(r * r - dist * dist) * Math.sign(h);
+        } else {
+            res = h - (dist - limR) * (limR / h);
+        }
+
+        return res;
+    }
+
+    function smoothFunc(a, b, k) {
+        let h = Math.max(0, Math.min(1, ((b - a) / k) + 0.5));
+        let m = h * (1 - h) * k;
+        return (h * a) + ((1 - h) * b) - (m * 0.5);
     }
 
     function onWindowResize() {
