70f2aa39d3aa6a66b2f5d8477ac4c31ca5659d5b
John Gerald Cayabyab <johngeraldcayabyab@gmail.com>
2021-07-15 19:42:54 +0800

    trying to make react three fiber work, fail

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import {BoxBufferGeometry, Group, Mesh, MeshStandardMaterial} from "three";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const group = new Group();
+    const geo = new BoxBufferGeometry(2, 2, 2);
+    const mat = new MeshStandardMaterial({color: 0x1fbeca});
+    const mesh = new Mesh(geo, mat);
+    group.add(mesh);
+    scene.add(group);
+
+    return (
+        <div className="App">
+
+            <Canvas>
+                <PointLight></PointLight>
+            </Canvas>
+
+            {/*<group>*/}
+            {/*    <mesh>*/}
+            {/*        <boxBufferGeometry attach="geometry" args={[0.047, 0.5, 0.29]}/>*/}
+            {/*        <meshStandardMaterial attach="material" color={0xf95b3c}/>*/}
+            {/*    </mesh>*/}
+            {/*</group>*/}
+
+            {/*<header className="App-header">*/}
+            {/*  /!*<img src={logo} className="App-logo" alt="logo" />*!/*/}
+            {/*  /!*<p>*!/*/}
+            {/*  /!*  Edit <code>src/App.js</code> and save to reload.*!/*/}
+            {/*  /!*</p>*!/*/}
+            {/*  /!*<a*!/*/}
+            {/*  /!*  className="App-link"*!/*/}
+            {/*  /!*  href="https://reactjs.org"*!/*/}
+            {/*  /!*  target="_blank"*!/*/}
+            {/*  /!*  rel="noopener noreferrer"*!/*/}
+            {/*  /!*>*!/*/}
+            {/*  /!*  Learn React*!/*/}
+            {/*  /!*</a>*!/*/}
+            {/*</header>*/}
+        </div>
+    );
 }
 
 export default App;
