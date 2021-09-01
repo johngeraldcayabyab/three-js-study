6c896216b27d160176a996fc62f964472afdac6a
Gerald <johngeraldcayabyab@gmail.com>
2021-09-01 21:53:25 +0800

    Still learning shaders

--- src/light_noise.js
+++ src/light_noise.js
@@ -71,7 +71,7 @@ function main() {
             let ldist = THREE.MathUtils.randFloat(1, 3);
             instData.push(x, z, scale);
             lucesInit.push(new THREE.Vector4(x, z, ldist, THREE.MathUtils.randFloat(1, 2)));
-            luces.push(new THREE.Vector4(x,z,scale, ldist))
+            luces.push(new THREE.Vector4(x, z, scale, ldist))
         }
         lg.setAttribute("instData", new THREE.InstancedBufferGeometry(new Float32Array(instData), 3));
         let lm = new THREE.MeshBasicMaterial({
@@ -141,24 +141,129 @@ function main() {
                         
                         vec3 displacedNormal = normalize(cross(displacedBitangent, displacedTangent));
                         return displacedNormal;
-                        
-                        ${shader.vertexShader}
-                    }
+          }
+          
+          
+          ${shader.vertexShader}
+        `.replace(
+                    `#include <begin_vertex>`
+                    , `
+                    #include <begin_vertex>
+                    
+                    float h = texture2D(noiseTex,uv).g;
+                    intensity = h;
+                    h = (h - 0.5) * 4.;
+                    vPos = transformed;
+                    transformedNormal = normalMatrix * getNormal(uv);
+                `);
+                shader.fragmentShader = `
+                    uniform vec4 luces[${instCount}];
+                    uniform sampler2D noiseTex;
+                    uniform float globalBloom;
+                    varying vec3 vPos;
+                    varying float intensity;
+                    
+                    ${shader.fragmentShader}
                 `.replace(
-                    `#include <begin_vertex>`,
-                    `#include <begin_vertex>
-                float h = texture2D(noiseTex, uv).g;
-                intensity = h;
-                h = (h - 0.5) * 4.;
-                transformed.y = h;
-                vPos = transformed;
-                transformedNormal = normalMatrix * getNormal(uv);
-`
-                 `   
+                    `#include <fog_fragment>`,
+                    `
+                        vec3 col = vec3(1,0,0)*0.75;
+                        float intensity = 0.;
+                        for(int i = 0; i < ${instCount}; i++){
+                            vec4 lux = luces[i];
+                            vec2 luxUv = (vec2(lux.x, -lux.y) - vec2(-25.)) / 50.;
+                            float h = texture2D(noiseTex, luxUv).g;
+                            h = (h - 0.5) * 4.;
+                            vec3 lightPos = vec3(lux.x, h, lux.y);
+                            float currIntensity = smoothstep(lux.z + lux.w, lux.z,
+                            distance(vPos, lightPos));
+                            intensity += pow(currIntensity, 16.);
+                        }
+                        intensity = clamp(intensity, 0.1, 1.);
+                        col = mix(col * 0.5, col, intensity);
+                        col = mix(gl_FragColor.rgb, col, intensity);
+                        col += vec3(1) * intensity * 0.01;
+                        gl_FragColor = vec4(col, opacity);
+                        #include <fog_fragment>
+                    `
+                ).replace(
+                    `#include <dithering_fragment>`,
+                    `#include <dithering_fragment>
+                                if(globalBloom > 0.5){
+                                    gl_FragColor = vec(0);
+                                }
+                            `
                 );
             }
-        })
+        });
 
+        let plane = new THREE.Mesh(pg, pm);
+        scene.add(plane);
+
+        let tg = new THREE.PlaneGeometry();
+        tg.translate(0, 0.5, 0);
+        tg.scale(5, 5, 5);
+        let tm = new THREE.MeshBasicMaterial({
+            color: 0xff4400,
+            fog: false,
+            transparent: true,
+            onBeforeCompile: shader => {
+                shader.uniforms.time = globalUniforms.time;
+                shader.uniforms.globalBloom = globalUniforms.globalBloom;
+                shader.fragmentShader = `
+                    #define S(a,b ,t) smoothstep(a,b,t)
+                    uniform float time;
+                    uniform float globalBloom;
+                    
+                    ${noise}
+                    
+                    float getTri(vec2 uv, float shift){
+                        uv = uv * 2.-1.;
+                        float a = atan(uv.x + shift,uv.y) + 3.1415926;
+                        float r = 3.1415926 * 2./3.;
+                        return cos(floor(.5+a/r)*r-a)*length(uv);
+                    } 
+                    
+                    float doubleTri(vec2 uv, float still, float width){
+                        vec2 baseUv = uv;
+                        vec2 e2 = fwidth(baseUv * 20.);
+                        float e = min(e2.x, e2.y) * width;
+                        float baseTri = getTri(baseUv, cos(baseUv.y * 31. + time) *
+                        sine(baseUv.y * 27. + time * 4.( * 0.025 * still);
+                        float d = abs(fract(baseTri * 20.) - 0.5);
+                        float tri = S(e, 0., td) - S(0., e, td);
+                        tri *= step(0.4, baseTri) - step(0.5, baseTri);
+                        return tri;
+                    }
+                    
+                    ${shader.fragmentShader}
+                `.replace(
+                    `vec4 diffuseColor = vec4(diffuse, opacity);`,
+                    `
+                        float tri = doubleTri(vUv, 0.0, 16.);
+                        float triWave = doubleTri(vUv, 1.0, 8.);
+                        float fullTri = max(tri, triWave);
+                        
+                        if(fullTri < 0.5) discard;
+                        
+                        vec3 col = mix(diffuse, vec3(0.75), fullTri);
+                        
+                        float blinking = smoothNoise(vec2(time, time * 5.));
+                        blinking = blinking * 0.9 + 0.1;
+                        
+                        vec4 diffuseColor = vec4(col * blinking, fullTri);
+                    `
+                ).replace(
+                    `#include <dithering_fragment>`,
+                    `#include <dithering_fragment>
+                    if(globalBloom > 0.5){
+                    
+                        gl_FragColor = vec4(gl_FragColor.rgb * 0.375, fullTri);
+                    }
+`
+                );
+            }
+        });
 
         clock = new THREE.Clock();
         renderer = createRenderer(renderer);
