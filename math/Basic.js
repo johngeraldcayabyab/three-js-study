a56b7dc3634a6c03c9249e8757e21b245ea0e616
Gerald <johngeraldcayabyab@gmail.com>
2021-09-17 23:14:11 +0800

    Added velocity in vector

--- math/Basic.js
+++ math/Basic.js
@@ -4,40 +4,56 @@ class Vector {
         this.y = y;
     }
 
+    getLength() {
+        return Math.sqrt((this.x * this.x) + (this.y * this.y));
+    }
+}
+
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
     addVector(vector) {
-        this.x = this.x + vector.x;
-        this.y = this.y + vector.y;
-        console.log({
-            x: this.x,
-            y: this.y,
-        });
+        return new Vector(this.x + vector.x, this.y + vector.y);
     }
 
     subtractVector(vector) {
-        this.x = this.x - vector.x;
-        this.y = this.y - vector.y;
-        console.log({
-            x: this.x,
-            y: this.y
-        })
+        return new Vector(this.x - vector.x, this.y - vector.y);
     }
 
-    getLength() {
-        this.length = Math.sqrt((this.x * this.x) + (this.y * this.y));
-        console.log({
-            length: this.length
-        })
+    doubled(s) {
+        return new Vector(this.x * s, this.y * s);
+    }
+
+    halfed(s) {
+        return new Vector(this.x / s, this.y / s);
     }
 }
 
 class Main {
     init() {
-        let pacman = new Vector(0, -1);
-        let inky = new Vector(1, 1);
-        let clyde = new Vector(2, -1);
-        pacman.subtractVector(inky);
-        let distance = new Vector(pacman.x, pacman.y);
-        distance.getLength();
+
+        let pacman = new Point(3,4);
+        console.log('pacmans initial speed is: ', (new Vector(pacman.x, pacman.y)).getLength());
+
+        let pacmanDoubled = pacman.doubled(2);
+
+        console.log('pacmans speed doubled is: ', (new Vector(pacmanDoubled.x, pacmanDoubled.y)).getLength());
+
+        let pacmanHalfed = pacman.halfed(2);
+
+        console.log('pacmans speed halfed is: ', (new Vector(pacmanHalfed.x, pacmanHalfed.y)).getLength());
+
+        // let pacman = new Point(0, -1);
+        // let inky = new Point(1, 1);
+        // let clyde = new Point(2, -1);
+        //
+        // let inkyToPacman = pacman.subtractVector(inky);
+        // let clydeToPacman = pacman.subtractVector(clyde);
+        //
+        // console.log(inkyToPacman.getLength(), clydeToPacman.getLength());
     }
 }
 
