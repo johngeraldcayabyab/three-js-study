c50eaf79225d89c03e7cff935492f7786e3c4ffa
John Gerald Cayabyab <johngeraldcayabyab@gmail.com>
2021-09-19 12:39:07 +0800

    Added normalization to vector

--- math/Basic.js
+++ math/Basic.js
@@ -7,9 +7,20 @@ class Vector {
     getLength() {
         return Math.sqrt((this.x * this.x) + (this.y * this.y));
     }
+
+    getNormalizedVector() {
+        let length = this.getLength();
+        this.x = this.x / length;
+        this.y = this.y / length;
+        return this;
+    }
+
+    getNormalizedLength() {
+        return this.getNormalizedVector().getLength();
+    }
 }
 
-class Point {
+class PointVector {
     constructor(x, y) {
         this.x = x;
         this.y = y;
@@ -23,36 +34,54 @@ class Point {
         return new Vector(this.x - vector.x, this.y - vector.y);
     }
 
-    doubled(s) {
-        return new Vector(this.x * s, this.y * s);
+    doubled(scalar) {
+        return new Vector(this.x * scalar, this.y * scalar);
     }
 
-    halfed(s) {
-        return new Vector(this.x / s, this.y / s);
+    halfed(scalar) {
+        return new Vector(this.x / scalar, this.y / scalar);
+    }
+
+    normalized() {
+        // let length =getLength();
+        // return this
     }
 }
 
 class Main {
     init() {
 
-        let pacman = new Point(3,4);
-        console.log('pacmans initial speed is: ', (new Vector(pacman.x, pacman.y)).getLength());
 
-        let pacmanDoubled = pacman.doubled(2);
-
-        console.log('pacmans speed doubled is: ', (new Vector(pacmanDoubled.x, pacmanDoubled.y)).getLength());
-
-        let pacmanHalfed = pacman.halfed(2);
-
-        console.log('pacmans speed halfed is: ', (new Vector(pacmanHalfed.x, pacmanHalfed.y)).getLength());
-
-        // let pacman = new Point(0, -1);
-        // let inky = new Point(1, 1);
-        // let clyde = new Point(2, -1);
+        /**
+         * code about speed
+         */
+        // let pacman = new Point(3,4);
+        // console.log('pacmans initial speed is: ', (new Vector(pacman.x, pacman.y)).getLength());
         //
-        // let inkyToPacman = pacman.subtractVector(inky);
+        // let pacmanDoubled = pacman.doubled(2);
+        //
+        // console.log('pacmans speed doubled is: ', (new Vector(pacmanDoubled.x, pacmanDoubled.y)).getLength());
+        //
+        // let pacmanHalfed = pacman.halfed(2);
+        //
+        // console.log('pacmans speed halfed is: ', (new Vector(pacmanHalfed.x, pacmanHalfed.y)).getLength());
+
+
+        /**
+         * Code about length vector
+         */
+        let pacman = new PointVector(3, 4);
+        let inky = new PointVector(1, 2);
+        // let clyde = new PointVector(2, -1);
+
+        let inkyToPacmanLength = pacman.subtractVector(inky);
+
+
+        console.log(inkyToPacmanLength.getNormalizedVector());
+        console.log(inkyToPacmanLength.getNormalizedLength());
+
         // let clydeToPacman = pacman.subtractVector(clyde);
-        //
+
         // console.log(inkyToPacman.getLength(), clydeToPacman.getLength());
     }
 }
