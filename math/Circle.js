5b58a4485808bc9c2fb0fb0473ea04c417bd6e55
Gerald <johngeraldcayabyab@gmail.com>
2021-10-07 23:55:46 +0800

    relearning math

--- math/Circle.js
+++ math/Circle.js
@@ -19,7 +19,7 @@ class Circle {
     }
 
     getRadius() {
-        Math.sin()
+
     }
 
 }
---
+++ math/RightTriangle.js
@@ -0,0 +1,28 @@
+class RightTriangle {
+
+    square(number, exponent) {
+        let newNumber = number;
+
+        for (let i = 0; i <= exponent; i++) {
+            newNumber = newNumber * number;
+        }
+
+        return newNumber;
+    }
+
+    pythagorean() {
+        // return
+    }
+
+    sin() {
+
+    }
+
+    cos() {
+
+    }
+
+    tan() {
+
+    }
+}
